24d8fa14ff2c853c819bb2a850b4e6a90fc7a39a
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-04-19 08:27:30 -0400

    02 - fixed bug

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -10,12 +10,24 @@ function setDate() {
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
+    if (secondsDegrees === 90) {
+        secondHand.style.transition = 'all 0s';
+    } else {
+        secondHand.style.transition = 'all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)';
+    }
+
     // Minutes
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360 ) + 90;
     minHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-    // Hour
+    if (minutesDegrees === 90) {
+        mindHand.style.transition = 'all 0s';
+    } else {
+        minHand.style.transition = 'all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)';
+    }
+
+    // Hours
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;    
